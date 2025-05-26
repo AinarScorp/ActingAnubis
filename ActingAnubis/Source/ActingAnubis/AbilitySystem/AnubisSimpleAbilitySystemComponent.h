8ac6925bbe188f24685dd1b443d0e270ebe71a54
AinarScorp <Einarhallik@gmail.com>
2025-05-26 14:48:32 +0200

    Exposed a thing

--- ActingAnubis/Source/ActingAnubis/AbilitySystem/AnubisSimpleAbilitySystemComponent.h
+++ ActingAnubis/Source/ActingAnubis/AbilitySystem/AnubisSimpleAbilitySystemComponent.h
@@ -22,18 +22,18 @@ public:
 	TArray<TObjectPtr<UAbilitySet>> AbilitySets;
 
 	// Attribute sets to grant when this ability set is granted.
-	UPROPERTY(EditDefaultsOnly, Category = "Acting Anubis|No Ability Set Setup")
+	UPROPERTY(EditAnywhere, Category = "Acting Anubis|No Ability Set Setup")
 	TArray<TSubclassOf<UAttributeSet>> GrantedAttributes;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Acting Anubis|No Ability Set Setup")
+	UPROPERTY(EditAnywhere, Category = "Acting Anubis|No Ability Set Setup")
 	FGameplayTagContainer GrantedGameplayTags;
 
 	// Gameplay abilities to grant when this ability set is granted.
-	UPROPERTY(EditDefaultsOnly, Category = "Acting Anubis|No Ability Set Setup")
+	UPROPERTY(EditAnywhere, Category = "Acting Anubis|No Ability Set Setup")
 	TArray<TSubclassOf<UAnubisGameplayAbility>> GrantedGameplayAbilities;
 
 	// Gameplay effects to grant when this ability set is granted.
-	UPROPERTY(EditDefaultsOnly, Category = "Acting Anubis|No Ability Set Setup")
+	UPROPERTY(EditAnywhere, Category = "Acting Anubis|No Ability Set Setup")
 	TArray<TSubclassOf<UGameplayEffect>> GrantedGameplayEffects;
 private:
 	void GiveToAbilitySystem();
